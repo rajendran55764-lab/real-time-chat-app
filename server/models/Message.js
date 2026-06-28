a861993f2921acf44d8025f251d3374c843301a8
rajendran55764-lab <rajendran55764@gmail.com>
2026-06-28 18:25:11 +0530

    Create Message.js

---
+++ server/models/Message.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const MessageSchema = new mongoose.Schema({
+  room: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Room',
+    required: true
+  },
+  sender: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  senderName: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    enum: ['text', 'image', 'file'],
+    default: 'text'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Message', MessageSchema);
