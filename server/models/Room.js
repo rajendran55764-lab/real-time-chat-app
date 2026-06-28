2d9c45af51554d16ea49ecab6b4c149b3abf1621
rajendran55764-lab <rajendran55764@gmail.com>
2026-06-28 18:24:18 +0530

    Create Room.js

---
+++ server/models/Room.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const RoomSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  description: {
+    type: String,
+    default: ''
+  },
+  type: {
+    type: String,
+    enum: ['public', 'private'],
+    default: 'public'
+  },
+  members: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Room', RoomSchema);
