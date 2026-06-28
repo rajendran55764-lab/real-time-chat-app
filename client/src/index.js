468b5988c58b00ce94cbb7921d5d4b7c9b545e3a
rajendran55764-lab <rajendran55764@gmail.com>
2026-06-28 23:07:29 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import './App.css';
+
+function App() {
+  return (
+    <div style={{
+      background:'white',
+      padding:'40px',
+      margin:'50px auto',
+      maxWidth:'400px',
+      borderRadius:'15px',
+      textAlign:'center'
+    }}>
+      <h1>💬 Chat App Working!</h1>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
